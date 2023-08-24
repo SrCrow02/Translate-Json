e341d357072d9ebca2a1c88dff1a66bbdd896445
Crowler <88294753+SrCrow02@users.noreply.github.com>
2023-08-24 19:00:42 -0300

    Agora funciona com estrutura aninhada de json

--- index.js
+++ index.js
@@ -1,98 +1,125 @@
-import fs from "fs";
+import fs from "fs/promises";
 import inquirer from 'inquirer';
-import translate from 'translate-google';
+import translate from 'translate';
 import path from "path";
 
-const textoParaTraduzir = 'hello';
-const caminho = "./enterDir/ata.json"
-const output = "./output/tr.json"
+translate.engine = 'google';
+translate.key = 'SUA_CHAVE_DA_API_DO_GOOGLE_TRANSLATE'; 
+
+const pastaEntrada = "./enterDir/";
+const pastaSaida = "./translateFile/";
 let idiomaDestino = "";
 
-// Defina as opções do menu
 const menuOptions = [
-    {
-      type: 'list', // O tipo de pergunta é uma lista
-      name: 'menuItem', // Nome da resposta
-      message: 'Escolha uma opção:', // Pergunta
-      choices: [
-        'Portuguese',
-        'English',
-        'Spanish',
-        'Sair'
-      ]
-    }
-  ];
-
-  async function showMenu() {
-    const answer = await inquirer.prompt(menuOptions);
-    
-    switch (answer.menuItem) {
-      case 'Portuguese':
-        idiomaDestino = "pt";
-        break;
-      case 'English':
-        idiomaDestino = "en";
-        break;
-      case 'Spanish':
-        idiomaDestino = "es";
-        break;
-      case 'Sair':
-        console.log('Saindo do menu.');
-        return;
-    }
-  
-    // Mostra o menu novamente para continuar a escolher
-    await translateJson(caminho, idiomaDestino, output);
+  {
+    type: 'list',
+    name: 'menuItem',
+    message: 'Escolha uma opção:',
+    choices: [
+      'Portuguese',
+      'English',
+      'Spanish',
+      'Sair'
+    ]
   }
-  // Inicializa o menu
-  showMenu();
+];
 
-  async function translateJson(caminho, idiomaDestino, output){
-    try{
-        // Ler o conteúdo do arquivo JSON original
-        const contentOriginal = await fs.promises.readFile(caminho, "utf8");
+async function showMenu() {
+  const answer = await inquirer.prompt(menuOptions);
+  
+  switch (answer.menuItem) {
+    case 'Portuguese':
+      idiomaDestino = "pt";
+      break;
+    case 'English':
+      idiomaDestino = "en";
+      break;
+    case 'Spanish':
+      idiomaDestino = "es";
+      break;
+    case 'Sair':
+      console.log('Saindo do menu.');
+      return;
+  }
 
-        // Analisar o conteúdo JSON
-        const objetoJSON = JSON.parse(contentOriginal);
+  await translateAllJsonFiles(pastaEntrada, pastaSaida, idiomaDestino);
+}
 
-        async function traduzirObjeto(objeto, idiomaDestino){
-            const traducoes = {}
-            for(const chave in objeto) {
-                if(objeto.hasOwnProperty(chave)){
-                    const valor = objeto[chave];
-        
-                    try{
-                        const traducao = await translate(valor, { to: idiomaDestino });
-                        traducoes[chave] = traducao; // Criar uma nova propriedade no objeto traducoes
-                    } catch(err) {
-                        console.log("Erro ao traduzir: " + err)
-                        traducoes[chave] = valor; // Manter original caso dê erro
-                    }
-                }
-            }
-            return traducoes;
+// Inicializa o menu
+showMenu();
+
+async function verifyFolder() {
+  try {
+    await fs.stat(pastaSaida)
+  } catch{
+    fs.mkdir(pastaSaida)
+  }
+}
+
+async function translateAllJsonFiles(inputFolder, outputFolder, language) {
+  verifyFolder();
+
+  try {
+    const filesInDir = await fs.readdir(inputFolder);
+
+    for (const file of filesInDir) {
+      const filePath = path.join(inputFolder, file);
+      const stat = await fs.stat(filePath);
+
+      if (stat.isDirectory()) {
+        const newInputFolder = path.join(inputFolder, file);
+        const newOutputFolder = path.join(outputFolder, file);
+
+        try {
+          await fs.mkdir(newOutputFolder, { recursive: true });
+        } catch (err) {
+          // Ignora erro caso o diretório já exista
         }
 
-    // Executar a função e guardar objeto
-    const traducoes = await traduzirObjeto(objetoJSON, idiomaDestino);
-    
-    // Escrever as traduções no arquivo JSON traduzido
-    await fs.promises.writeFile(output, JSON.stringify(traducoes, null, 2));
+        await translateAllJsonFiles(newInputFolder, newOutputFolder, language);
+      } else if (path.extname(file) === '.json') {
+        const content = await fs.readFile(filePath, "utf8");
+        const jsonObject = JSON.parse(content);
 
-    console.log("Traduzido com sucesso")
-    } catch (err){
-        console.log(err)
+        // Verifica se há um objeto "content" no JSON
+        if (jsonObject.content && typeof jsonObject.content === "object") {
+          jsonObject.content = await translateJsonObject(jsonObject.content, language);
+        } else {
+          await translateJsonObject(jsonObject, language);
+        }
+
+        const translatedFileName = path.basename(file, '.json') + '_translated.json';
+        const outputPath = path.join(outputFolder, translatedFileName);
+        await fs.writeFile(outputPath, JSON.stringify(jsonObject, null, 2));
+
+        console.log(`Arquivo traduzido: ${translatedFileName}`);
+      }
+    }
+
+    console.log("Tradução de todos os arquivos concluída.");
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+}
+
+async function translateJsonObject(jsonObject, language) {
+  const translatedObject = {};
+  for (const chave in jsonObject) {
+    if (jsonObject.hasOwnProperty(chave)) {
+      const valor = jsonObject[chave];
+
+      if (typeof valor === "object" && valor !== null) {
+        translatedObject[chave] = await translateJsonObject(valor, language);
+      } else {
+        try {
+          const traducao = await translate(valor, { to: language });
+          translatedObject[chave] = traducao;
+        } catch (err) {
+          console.log("Erro ao traduzir: " + err);
+          translatedObject[chave] = valor;
+        }
+      }
     }
   }
-
-
-  /*async function translateFile(){
-    translate(textoParaTraduzir, { to: idiomaDestino })
-    .then((res) => {
-      console.log('Texto original:', textoParaTraduzir);
-      console.log('Texto traduzido:', res);
-    })
-    .catch((err) => {
-      console.error('Erro ao traduzir:', err);
-    });
-  }*/
+  return translatedObject;
+}
